b4b3f62fa5ba5611965efefb75f4453d76e3c1c5
xiby <15555438336@163.com>
2021-05-22 16:55:11 +0800

    [新增代码](服务器)添加服务器训练逻辑

--- src/Main.py
+++ src/Main.py
@@ -4,28 +4,20 @@
 from FederatedDataset import FederatedDataset
 
 import torch.nn as nn
-from torch.utils.data import DataLoader
-if __name__ == "__main__":
-    dataset = FederatedDataset(root='../datas/UCI_DATASET', subjects=[26])
-    dataloader = DataLoader(dataset, batch_size=64)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to('cpu'), y.to('cpu')
-        # print(X.shape)
-        X = X.unsqueeze(1)
-        conv1 = nn.Conv1d(1, 1, 9)
-        mp1 = nn.MaxPool1d(3)
-        out = conv1(X)
-        out = mp1(out)
-        out = conv1(out)
-        out = mp1(out)
 
-        fc1 = nn.Linear(58, 27)
-        out = fc1(out)
-        fc2 = nn.Linear(27, 6)
-        out = fc2(out)
-        out = out.squeeze(1)
-        sm = nn.Softmax(dim=1)
-        out = sm(out)
-        print(out.argmax(1))
-        print(y)
-    print("Main")
+from torch.utils.data import DataLoader
+from Model import Model
+
+def getClientFrozenModel():
+    model = Model()
+    for para in model.conv1.parameters():
+        para.requires_grad = False
+    for para in model.mp1.parameters():
+        para.requires_grad = False
+    for para in model.conv2.parameters():
+        para.requires_grad = False
+    for para in model.mp2.parameters():
+        para.requires_grad = False
+    return model
+if __name__ == "__main__":
+    print("Main starts")
--- src/Server.py
+++ src/Server.py
@@ -1,7 +1,36 @@
 '''
 服务端实现
 '''
+import torch
+
 from base.BaseServer import BaseServer
 class Server(BaseServer):
-    def __init__(self, model, testDataloader=None):
+    def __init__(self, model, dataloader, optimizer, loss_fn, testDataloader=None):
         super().__init__(model, testDataloader)
+        self.dataloader = dataloader
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.device = "cuda" if torch.cuda.is_available() else "cpu" 
+    def trainModel(self):
+        size = len(self.dataloader.dataset)
+        for batch, (X, y) in enumerate(self.dataloader):
+            X, y = X.to(self.device), y.to(self.device)
+            pred = self.model(X)
+            loss = self.loss_fn(pred, y)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            if batch % 10 == 0:
+                loss, current = loss.item(), (batch + 1) * len(X)
+                print(f"server batch: {batch:>5d}  loss:{loss:>7f} [{current:>5d}/{size:>5d}]")
+    def aggregate(self):
+        '''
+        TODO server update its model by aligning with user model
+        '''
+        pass
+    def testModel(self):
+        '''
+        全局模型不需要测试
+        '''
+        pass
+
