ae5e436cefcbaa08726409fcfb8170181c6f8ed2
xiby <15555438336@163.com>
2021-05-24 21:58:23 +0800

    [新增代码](代办)新增模型训练待办

--- src/Client.py
+++ src/Client.py
@@ -14,6 +14,9 @@ class Client(BaseClient):
         return self.model.state_dict()
     
     def trainModel(self):
+        '''
+        TODO 论文中对于客户端有两种训练方法，一种是一开始利用自身数据进行训练，第二种是训练迁移后的训练，需要思考怎么实现
+        '''
         pass
 
     def testModel(self):
