08b04264cd10d420c81a95e173950c60c2889b4d
xiby <15555438336@163.com>
2021-05-24 21:33:34 +0800

    [新增代码](客户端实现)添加客户端两个实现函数

--- src/Client.py
+++ src/Client.py
@@ -6,3 +6,16 @@ from base.BaseClient import BaseClient
 class Client(BaseClient):
     def __init__(self, model, optimizer, loss_fn, dataloader, testDataloader=None):
         super().__init__(model, optimizer, loss_fn, dataloader, testDataloader)
+    
+    def loadParams(self, param):
+        self.model.load_state_dict(param)
+
+    def reportParams(self):
+        return self.model.state_dict()
+    
+    def trainModel(self):
+        pass
+
+    def testModel(self):
+        pass
+    
